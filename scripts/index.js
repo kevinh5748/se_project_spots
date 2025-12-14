032ae07a6afa4fa03679bb0d39623245f47cee91
Kevin Hinkston <kevin_hinkston@yahoo.com>
2025-12-14 07:23:58 -0800

    Project 4 Stage 2. Opening and Closisng the Modals

---
+++ scripts/index.js
@@ -0,0 +1,27 @@
+// Select elements
+
+const editProfileBtn = document.querySelector(".profile__edit-btn");
+const editProfileModal = document.querySelector("#edit-profile-modal");
+const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+
+const newPostBtn = document.querySelector(".profile__new-post-btn");
+const newPostModal = document.querySelector("#new-post-modal");
+const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
+
+// Open modal when edit button is clicked
+editProfileBtn.addEventListener("click", function() {
+  editProfileModal.classList.add("modal_is-opened");
+});
+
+// Close modal when close button is clicked
+editProfileClosedBtn.addEventListener("click", function() {
+  editProfileModal.classList.remove("modal_is-opened");
+});
+
+newPostBtn.addEventListener("click", function() {
+  newPostModal.classList.add("modal_is-opened");
+});
+
+newPostClosedBtn.addEventListener("click", function() {
+  newPostModal.classList.remove("modal_is-opened");
+});

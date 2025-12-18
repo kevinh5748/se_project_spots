125205d6294fa371d1ae3f095733500a781a51b4
Kevin Hinkston <kevin_hinkston@yahoo.com>
2025-12-18 00:41:52 -0800

    (SE Part-time) Spots Stage 5 Part 2 - Edit Profile Form Submission

--- scripts/index.js
+++ scripts/index.js
@@ -4,12 +4,24 @@ const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
 
+const editProfileForm = editProfileModal.querySelector{".modal__form"};
+const editProfileNameInput = editProfileModal.querySelector("#profile-name-input");
+
+const editProfileDescriptionInput = editProfileModal.querySelector("#profile-Description-input");
+
 const newPostBtn = document.querySelector(".profile__add-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 
+const profileNameEl = document.querySelector{".profile__name"};
+const profileDescriptionEl = document.querySelector{".profile__description"};
+
 // Open modal when edit button is clicked
 editProfileBtn.addEventListener("click", function() {
+  editProfileNameInput.value = profileNameEl.textContent;
+  // TODO Fill description input
+editProfileDescriptionInput.value = profileDescriptionEl.textContent;
+
   editProfileModal.classList.add("modal_is-opened");
 });
 
@@ -25,3 +37,13 @@ newPostBtn.addEventListener("click", function() {
 newPostCloseBtn.addEventListener("click", function() {
   newPostModal.classList.remove("modal_is-opened");
 });
+
+function handleEditProfileSubmit (evt) {
+  evt.preventDefault();
+  profileNameEl.textContent = editProfileNameInput.value;
+  profileDescriptionEl.textContent = editProfileDescriptionInput.value;
+
+  newPostModal.classList.remove("modal_is-opened");
+}
+
+editProfileForm.addEventListener("submit" , handleEditProfileSubmit);

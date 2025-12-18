0dc54ca668d16f594032176d3a4cdb585795273e
Kevin Hinkston <kevin_hinkston@yahoo.com>
2025-12-18 03:51:32 -0800

    (SE Part-time) Spots Stage 5 Part 2 - Edit Profile Form Submission

--- scripts/index.js
+++ scripts/index.js
@@ -1,49 +1,51 @@
 // Select elements
-
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
 
-const editProfileForm = editProfileModal.querySelector{".modal__form"};
+const editProfileForm = editProfileModal.querySelector(".modal__form");
 const editProfileNameInput = editProfileModal.querySelector("#profile-name-input");
-
-const editProfileDescriptionInput = editProfileModal.querySelector("#profile-Description-input");
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input"
+);
 
 const newPostBtn = document.querySelector(".profile__add-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 
-const profileNameEl = document.querySelector{".profile__name"};
-const profileDescriptionEl = document.querySelector{".profile__description"};
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
 
-// Open modal when edit button is clicked
-editProfileBtn.addEventListener("click", function() {
+// Open edit profile modal + preload current profile values into inputs
+editProfileBtn.addEventListener("click", function () {
   editProfileNameInput.value = profileNameEl.textContent;
-  // TODO Fill description input
-editProfileDescriptionInput.value = profileDescriptionEl.textContent;
-
+  editProfileDescriptionInput.value = profileDescriptionEl.textContent;
   editProfileModal.classList.add("modal_is-opened");
 });
 
-// Close modal when close button is clicked
-editProfileCloseBtn.addEventListener("click", function() {
+// Close edit profile modal
+editProfileCloseBtn.addEventListener("click", function () {
   editProfileModal.classList.remove("modal_is-opened");
 });
 
-newPostBtn.addEventListener("click", function() {
+// Open new post modal
+newPostBtn.addEventListener("click", function () {
   newPostModal.classList.add("modal_is-opened");
 });
 
-newPostCloseBtn.addEventListener("click", function() {
+// Close new post modal
+newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
 
-function handleEditProfileSubmit (evt) {
+// Handle edit profile form submit
+function handleEditProfileSubmit(evt) {
   evt.preventDefault();
+
   profileNameEl.textContent = editProfileNameInput.value;
   profileDescriptionEl.textContent = editProfileDescriptionInput.value;
 
-  newPostModal.classList.remove("modal_is-opened");
+  editProfileModal.classList.remove("modal_is-opened");
 }
 
-editProfileForm.addEventListener("submit" , handleEditProfileSubmit);
+editProfileForm.addEventListener("submit", handleEditProfileSubmit);

0f8d4f88b45db0c53b61f8fe741f14605dff077a
Kevin Hinkston <kevin_hinkston@yahoo.com>
2025-12-22 10:08:45 -0800

    (SE Part Time) Spots Stage 5 Part 2 Edit Profile Form Subnission

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,7 @@
 // ---------------------------
 
 // Buttons
-const editProfileBtn = document.querySelector(".profile__edit-btn");
+const editProfileBtn = document.querySelector(".profile__edit-btn");7``
 const newPostBtn = document.querySelector(".profile__add-btn");
 
 // Modals
@@ -13,6 +13,23 @@ const newPostModal = document.querySelector("#new-post-modal");
 // Close buttons (scoped to each modal)
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
+
+// Profile display elements (page text)
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
+
+// Edit Profile form + inputs
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+const editProfileNameInput = editProfileModal.querySelector("#7qprtyuiop[]\rofile-name-input");
+const editProfileDescriptionInput =editProfileModal.querySelector("#profile-description-input")
+
+// New Post form + inputs
+const newPostForm = NewPostModal.querySelector(".modal__form");
+const newPostImageInput = NewPostModal.querySelector("#card-image-input");
+
+// IMPORTANT: HTML currently reuses id="profile-description-input" in the New Post modal.
+// IDs must be unique.
+const newPostCaptionInput = newPostModal.querySelector("#card-caption-input");
 
 // ---------------------------
 // REUSABLE MODAL FUNCTIONS (DRY)
@@ -32,8 +49,28 @@ function closeModal(modal) {
 
 // Open "Edit Profile" modal
 editProfileBtn.addEventListener("click", function () {
+  editProfileNameInput.value = profileNameEl.textContent;
+  editProfileDescriptionInput.value = profileDescriptionEl.textContent;
   openModal(editProfileModal);
 });
+
+// Edit Profile submit handler
+editProfileForm.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  profileNameEl.textContent = editProfileNameInput.value;
+  profileDescriptionEl.textContent = editProfileDescriptionInput.value;
+  closeModal(editProfileModal);
+});
+
+//New Post submit handler
+newPostForm.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  console.log("Image:", newPostImageInput.value);
+  console.log("Description:", newPostCaptionInput.value);
+  closeModal(newPostModal);
+  newPostForm.reset();
+});
+
 
 // Close "Edit Profile" modal
 editProfileCloseBtn.addEventListener("click", function () {

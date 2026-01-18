d6bf9a6fc0be2ebb558960e2712e9c727bb0ccfd
Kevin Hinkston <kevin_hinkston@yahoo.com>
2026-01-18 03:33:34 -0800

    Spots Stage 6. Loopingg Through an Array of Objects

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,31 @@
+const initialCards = [
+  {
+    name: "Val Thorens",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg"
+  },
+  {
+    name: "Restaurant terrace",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg"
+  },
+  {
+    name: "Restaurant terraceAn outdoor cafe",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg"
+  },
+  {
+    name: "A very long bridge, over the forest and through the trees",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg"
+  },
+  {
+    name: "Tunnel with morning light",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg"
+  },
+  {
+    name: "Mountain house",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg"
+  }
+];
+
+
 // ---------------------------
 // DOM SELECTIONS
 // ---------------------------
@@ -85,4 +113,9 @@ newPostBtn.addEventListener("click", function () {
 // Close "New Post" modal
 newPostCloseBtn.addEventListener("click", function () {
   closeModal(newPostModal);
+});
+
+initialCards.forEach(function (card) {
+  console.log(card.name);
+  console.log(card.link);
 });
